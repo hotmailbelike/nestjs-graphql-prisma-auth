2cb87b4424626353b9e861b26c385fa890c812f9
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-11 23:15:30 +0600

    Create signup-input.ts
    feat: define sign up input

---
+++ src/auth/dto/signup-input.ts
@@ -0,0 +1,20 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+@InputType()
+export class SignUpInput {
+  @IsNotEmpty()
+  @IsString()
+  @Field()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field()
+  password: string;
+}
