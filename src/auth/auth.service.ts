c86a604368ec64da97c91f940ce52c7f93ede39c
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-12 00:12:40 +0600

    Update auth.service.ts
    feat: sign up, create token function, and update token function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { SignUpInput } from './dto/signup-input';
 import { UpdateAuthInput } from './dto/update-auth.input';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
-  create(signUpInput: SignUpInput) {
-    return 'This action adds a new auth';
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async signup(signUpInput: SignUpInput) {
+    const hashedPassword = await hash(signUpInput.password);
+    const user = await this.prisma.user.create({
+      data: {
+        name: SignUpInput.name,
+        password: hashedPassword,
+        email: signUpInput.email,
+      },
+    });
+
+    const { accessToken, refreshToken } = await this.createTokens(
+      user.id,
+      user.email,
+    );
+
+    await this.updateRefreshToken(user.id, refreshToken);
+
+    return {
+      accessToken,
+      refreshToken,
+      user,
+    };
   }
 
   findAll() {
@@ -22,5 +52,44 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  async createTokens(userId: number, email: string) {
+    const accessToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+      },
+      {
+        expiresIn: '10s',
+        secret: this.configService.get('ACCESS_TOKEN_SECRET'),
+      },
+    );
+
+    const refreshToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+        accessToken,
+      },
+      {
+        expiresIn: '7d',
+        secret: this.configService.get('REFRESH_TOKEN_SECRET'),
+      },
+    );
+
+    return { accessToken, refreshToken };
+  }
+
+  async updateRefreshToken(userId: number, refreshToken: string) {
+    const hashedRefreshToken = await hash(refreshToken);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        refreshToken: hashedRefreshToken,
+      },
+    });
   }
 }
