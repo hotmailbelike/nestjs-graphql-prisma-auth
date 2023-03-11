d107f33e928825bbb25c59822bf1ac4768b7fcaa
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-11 21:16:11 +0600

    Update app.module.ts
    fix: remove dependencies from delete files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -14,7 +12,7 @@ import { PrismaService } from './prisma/prisma.service';
       sortSchema: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
