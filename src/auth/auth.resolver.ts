77fae58958aaafc73b3c2ac4094b8b206f877153
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-11 23:12:55 +0600

    feat: created auth resource with `nest g resource auth` command

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,36 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { Auth } from './entities/auth.entity';
+import { SignUpInput } from './dto/signup-input';
+import { UpdateAuthInput } from './dto/update-auth.input';
+import { AuthResponse } from './dto/auth-response';
+
+@Resolver(() => Auth)
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => AuthResponse)
+  signup(@Args('signUpInput') signUpInput: SignUpInput) {
+    return this.authService.create(signUpInput);
+  }
+
+  @Query(() => [Auth], { name: 'auth' })
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Query(() => Auth, { name: 'auth' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.authService.findOne(id);
+  }
+
+  @Mutation(() => Auth)
+  updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
+    return this.authService.update(updateAuthInput.id, updateAuthInput);
+  }
+
+  @Mutation(() => Auth)
+  removeAuth(@Args('id', { type: () => Int }) id: number) {
+    return this.authService.remove(id);
+  }
+}
